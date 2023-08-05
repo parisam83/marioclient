2db4a3add9ca921ef9d048274b04c886ec5d905a
Parisa <mousaviparisa5@yahoo.com>
2023-08-05 17:58:38 +0330

    Add another ButtonCreator constructor for ShopPage

--- src/main/java/com/parim/view/swingObjects/ButtonCreator.java
+++ src/main/java/com/parim/view/swingObjects/ButtonCreator.java
@@ -4,6 +4,7 @@ import com.parim.view.MainFrame;
 import com.parim.view.loaders.FontLoader;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ButtonCreator extends JButton {
     public ButtonCreator(int y, String text){
@@ -18,6 +19,26 @@ public class ButtonCreator extends JButton {
         this.setText(text);
         this.setFont(FontLoader.buttonFont);
         this.setBounds(x, y, 300, 100);
+        this.setFocusable(false);
+        this.setHorizontalTextPosition(JButton.CENTER);
+        this.setVerticalTextPosition(JButton.BOTTOM);
+    }
+    public ButtonCreator(int x, int y, Image image, String name, String price){
+        this.setIcon(new ImageIcon(image));
+        this.setText(name + " :: " + price);
+        this.setBounds(x, y, 250, 350);
+        this.setFont(FontLoader.notificationFont);
+        this.setFocusable(false);
+        this.setHorizontalTextPosition(JButton.CENTER);
+        this.setVerticalTextPosition(JButton.BOTTOM);
+    }
+
+    public ButtonCreator(int x, int y, Image image1, Image image2, String name1, String name2, String price){
+        this.setIcon(new TwoIconCreator(new ImageIcon(image1), new ImageIcon(image2)));
+        this.setText(MainFrame.getInstance().createText(name1 + "\n" + name2, price));
+        System.out.println(this.getText());
+        this.setBounds(x, y, 250, 350);
+        this.setFont(FontLoader.notificationFont);
         this.setFocusable(false);
         this.setHorizontalTextPosition(JButton.CENTER);
         this.setVerticalTextPosition(JButton.BOTTOM);
